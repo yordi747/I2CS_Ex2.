9c081fe3b7dc2311490c14ea12b509cd3c3511a1
yordi747 <yordanossema@gmail.com>
2025-12-28 17:22:27 +0200

    fixing

--- src/assignment/Index2DTest.java
+++ src/assignment/Index2DTest.java
@@ -1,39 +1,59 @@
 package assignment;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-    public class Index2DTest {
+public class Index2DTest {
 
-        @Test
-        void testGetters() {
-            Pixel2D p = new Index2D(3, 7);
-            assertEquals(3, p.getX());
-            assertEquals(7, p.getY());
-        }
-
-        @Test
-        void testDistance() {
-            Pixel2D p1 = new Index2D(0, 0);
-            Pixel2D p2 = new Index2D(3, 4);
-            assertEquals(5.0, p1.distance2D(p2), 1e-9);
-        }
-
-        @Test
-        void testEquals() {
-            Pixel2D p1 = new Index2D(2, 2);
-            Pixel2D p2 = new Index2D(2, 2);
-            Pixel2D p3 = new Index2D(2, 3);
-
-            assertEquals(p1, p2);
-            assertNotEquals(p1, p3);
-            assertNotEquals(p1, null);
-        }
-
-        @Test
-        void testToString() {
-            Pixel2D p = new Index2D(5, 9);
-            assertEquals("5,9", p.toString());
-        }
+    @Test
+    public void testGetters() {
+        Index2D p = new Index2D(3, 7);
+        assertEquals(3, p.getX());
+        assertEquals(7, p.getY());
     }
 
+    @Test
+    public void testCopyConstructor() {
+        Pixel2D a = new Index2D(1, 2);
+        Index2D b = new Index2D(a);
+        assertEquals(1, b.getX());
+        assertEquals(2, b.getY());
+        assertEquals(a, b);
+    }
+
+    @Test
+    public void testDistance2D() {
+        Pixel2D p1 = new Index2D(0, 0);
+        Pixel2D p2 = new Index2D(3, 4);
+        assertEquals(5.0, p1.distance2D(p2), 1e-9);
+    }
+
+    @Test
+    public void testDistance2DNull() {
+        Pixel2D p1 = new Index2D(0, 0);
+        assertThrows(IllegalArgumentException.class, () -> p1.distance2D(null));
+    }
+
+    @Test
+    public void testToString() {
+        Index2D p = new Index2D(10, 20);
+        assertEquals("10,20", p.toString());
+    }
+
+    @Test
+    public void testEqualsAndHashCode() {
+        Index2D a = new Index2D(5, 6);
+        Index2D b = new Index2D(5, 6);
+        Index2D c = new Index2D(5, 7);
+
+        assertEquals(a, b);
+        assertEquals(a.hashCode(), b.hashCode());
+
+        assertNotEquals(a, c);
+        assertNotEquals(a.hashCode(), c.hashCode());
+
+        assertNotEquals(a, null);
+        assertNotEquals(a, "5,6");
+    }
+}
